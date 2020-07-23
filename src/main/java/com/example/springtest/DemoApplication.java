8d66f3870dd1b491758b453f94b29115e546ef50
earlgreybutter <duckduck31216@gmail.com>
2020-07-23 12:26:46 +0900

    successmapping

--- src/main/java/com/example/springtest/DemoApplication.java
+++ src/main/java/com/example/springtest/DemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 실제로는 3개의 어노테이션으로 구성 
+// @SpringBootConfiguration
+// @EnableAutoConfiguration
+// @ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
